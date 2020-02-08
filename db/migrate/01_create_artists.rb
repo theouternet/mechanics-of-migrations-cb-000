3ca7ea489210c36f5dbfcd016befa320c6a4c93d
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-02-08 23:00:24 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -3,7 +3,7 @@ class CreateArtists < ActiveRecord::Migration[5.2]
       create_table :artists do |t|
         t.string :name
         t.string :genre 
-        t.string :name
-        t.string :name
+        t.string :age 
+        t.string :hometown
   end
 end
